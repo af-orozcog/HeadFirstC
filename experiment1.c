a715a5d2b67e5977755b37d828e1640ebb354b54
root <root@LAPTOP-NQ7KMOGB.localdomain>
2020-09-29 12:45:42 -0500

    add some experiments and examples from the book chapter 1 and 2

---
+++ experiment1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+	char message[] = "Don't call me";
+	puts((message+6));
+	return 0;
+}
---
+++ experiment2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+	char see[] = "Don't call me";
+	printf("the size is %ld\n",sizeof(see));
+	return 0;
+}
